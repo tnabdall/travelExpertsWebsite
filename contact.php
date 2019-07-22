988ac9b1b5bffb3e09d1f2694cd02f8c094b5c7f
Tarik Abdalla <826506@ACDM.DS.SAIT.CA>
2019-07-22 10:51:07 -0600

    Added Owiny's conctact page

--- contact.php
+++ contact.php
@@ -1,22 +1,220 @@
 <?php include("pageSections/header.php") ?>
+<script src="js/contact.js"></script>
 <main>
     <div class="rounded sectionBox">
-        <p class="row centered">Feel free to contact our agency by phone (403-200-0000), email
-            (travel@travelexperts.com), or by reaching out to any of our great agents.</p>
-        <dl class="container-fluid">
 
-            <dt><b>Tarik A - Head Travel Agent</b></dt>
-            <dd>Phone: 403-200-0001</dd>
-            <dd>Email: tarik.a@travelexperts.com</dd>
+        <section class="company-info">
+            <ul class="agency-info">
+                <!-- Company Contact Information -->
+                <h2><b>Agency Name: Classical Travel</b></h2>
+                <p>Address: 123 Love Street, Heaven</p>
+            </ul>
+        </section>
 
-            <dt><b>John Doe - International Travel Agent</b></dt>
-            <dd>Phone: 403-200-0002</dd>
-            <dd>Email: john.doe@travelexperts.com</dd>
+        <div class="ui five stackable cards">
 
-            <dt><b>Mary Jane - Domestic Travel Agent</b></dt>
-            <dd>Phone: 403-200-0003</dd>
-            <dd>Email: mary.jane@travelexperts.com</dd>
-        </dl>
+            <div id="contactCard1" class="ui card">
+                <!-- MODAL CODE -->
+                <div id="modal1" class="ui modal test">
+                    <div class="header">
+                        Agent Contact Information
+                    </div>
+                    <div class="image content">
+                        <div class="ui small centered image">
+                            <img src="images/cards/EdwardTeach.jpg">
+                        </div>
+                        <div class="description">
+                            <div class="ui header">Edward Teach</div>
+                            <!-- Agent Contact Information -->
+                            <p>Phone#: 403-666-4141</p>
+                            <p>Email: e.teach@rrtravel.com</p>
+                        </div>
+                    </div>
+
+                    <div class="actions">
+                        <div class="ui black deny button">
+                            Back
+                        </div>
+                        <div class="ui positive right labeled icon button">
+                            <a href="contactAgent.html">Contact Agent</a>
+                            <!--Contact Agent
+                                <i class="checkmark icon"></i>-->
+                        </div>
+
+
+                    </div>
+                </div>
+                <!-- CONTACT CARD 1 -->
+                <div class="ui centered small image">
+                    <img id="teach" src="images/cards/EdwardTeach.jpg">
+                </div>
+                <div class="content">
+                    <a class="header">Edward Teach</a>
+                    <div class="meta">
+                        <span class="date">Senior Travel Agent</span>
+                    </div>
+                    <div class="description">
+                        Edward loves raiding and looting.
+                    </div>
+                </div>
+                <div class="extra">
+                    Rating:
+                    <div id="rating1" class="ui star rating"></div>
+                </div>
+            </div>
+
+
+            <div id="contactCard2" class="card">
+                <div class="image">
+                    <img>
+                </div>
+                <div class="extra">
+                    Rating:
+                    <div class="ui star rating">
+                    </div>
+                </div>
+            </div>
+
+            <div id="contactCard3" class="card">
+                <div class="image">
+                    <img>
+                </div>
+                <div class="extra">
+                    Rating:
+                    <div class="ui star rating"></div>
+                </div>
+            </div>
+
+            <div id="contactCard4" class="card">
+                <div class="image">
+                    <img>
+                </div>
+                <div class="extra">
+                    Rating:
+                    <div class="ui star rating"></div>
+                </div>
+            </div>
+
+            <div id="contactCard5" class="card">
+                <div class="image">
+                    <img>
+                </div>
+                <div class="extra">
+                    Rating:
+                    <div class="ui star rating"></div>
+                </div>
+            </div>
+        </div>
+
+        <section class="company-info">
+            <ul class="agency-info">
+                <!-- Company Contact Information -->
+                <h2><b>Agency Name: Rock & Roll Travel</b></h2>
+                <p>Address: 666 Heartbreak Blvd, Hell </p>
+            </ul>
+        </section>
+
+        <div class="ui five stackable cards">
+            <div id="contactCard6" class="card">
+                <div class="image">
+                    <img>
+                </div>
+                <div class="extra">
+                    Rating:
+                    <div class="ui star rating"></div>
+                </div>
+            </div>
+
+            <div id="contactCard" class="card">
+                <div class="image">
+                    <img>
+                </div>
+                <div class="extra">
+                    Rating:
+                    <div class="ui star rating"></div>
+                </div>
+            </div>
+
+            <div id="contactCard" class="card">
+                <div class="image">
+                    <img>
+                </div>
+                <div class="extra">
+                    Rating:
+                    <div class="ui star rating"></div>
+                </div>
+            </div>
+
+            <div id="contactCard" class="card">
+                <div class="image">
+                    <img>
+                </div>
+                <div class="extra">
+                    Rating:
+                    <div class="ui star rating"></div>
+                </div>
+            </div>
+
+            <div id="contactCard" class="card">
+                <div class="image">
+                    <img>
+                </div>
+                <div class="extra">
+                    Rating:
+                    <div class="ui star rating"></div>
+                </div>
+            </div>
+
+            <div id="contactCard" class="card">
+                <div class="image">
+                    <img>
+                </div>
+                <div class="extra">
+                    Rating:
+                    <div class="ui star rating"></div>
+                </div>
+            </div>
+
+            <div id="contactCard" class="card">
+                <div class="image">
+                    <img>
+                </div>
+                <div class="extra">
+                    Rating:
+                    <div class="ui star rating"></div>
+                </div>
+            </div>
+
+            <div id="contactCard" class="card">
+                <div class="image">
+                    <img>
+                </div>
+                <div class="extra">
+                    Rating:
+                    <div class="ui star rating"></div>
+                </div>
+            </div>
+
+            <div id="contactCard" class="card">
+                <div class="image">
+                    <img>
+                </div>
+                <div class="extra">
+                    Rating:
+                    <div class="ui star rating"></div>
+                </div>
+            </div>
+
+            <div id="contactCard" class="card">
+                <div class="image">
+                    <img>
+                </div>
+                <div class="extra">
+                    Rating:
+                    <div class="ui star rating" data-rating="2" data-max-rating="5"></div>
+                </div>
+            </div>
+        </div>
     </div>
 </main>
 <?php include ("pageSections/footer.php") ?>
