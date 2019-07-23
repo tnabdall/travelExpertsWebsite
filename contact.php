3920483a9cea2dc2f2c42f00a55ee2e70ecd2253
Owiny Ayorech <356777@ACDM.DS.SAIT.CA>
2019-07-23 14:04:34 -0600

    PHP Changes

--- contact.php
+++ contact.php
@@ -1,42 +1,110 @@
 <?php include("pageSections/header.php") ?>
-    <script src="js/contact.js"></script>
-    <main>
+<script src="js/contact.js"></script>
+<main>
     <div class="rounded sectionBox">
+
         <div class="ui five stackable cards">
+            
             <?php
-            require "classes/dbConnect.php";
-            // $conn = getDatabase();
-            $db = new Database();
-            $conn = $db -> getConn();
-            $sql = "SELECT `AgtFirstName`, `AgtLastName`, `AgtBusPhone`, `AgtEmail`, `AgtPosition`, `AgencyId`, `Rating`, `Description`, `Title`, `Image`  FROM `agents` WHERE 1;";
+                require "classes/dbConnect.php";
+                // $conn = getDatabase();
+                $db = new Database();
+                $conn = $db -> getConn();
+                $sql = "SELECT t1.`AgencyId`
+                , t1.`AgtFirstName`
+                , t1.`AgtLastName`
+                , t1.`AgtBusPhone`
+                , t1.`AgtEmail`
+                , t1.`AgtPosition`
+                , t1.`Rating`
+                , t1.`Description`
+                , t1.`Title`
+                , t1.`Image`
+                , t2.`AgncyName`
+                , t2.`AgncyAddress`
+                , t2.`AgncyProv`
+                , t2.`AgncyPhone`
+                FROM agents t1
+                JOIN agencies t2
+                ON t2.`AgencyId` = t1.`AgencyId`
+                WHERE 1";
 
-            $result = $conn->query($sql);
+                $result = $conn->query($sql);
 
-            if ($result === false) {
-                var_dump($conn->errorInfo());
-             } else {
-                $agents = $result->fetchAll(PDO::FETCH_ASSOC);
-             }
+                if ($result === false) {
+                    var_dump($conn->errorInfo());
+                } else {
+                    $agents = $result->fetchAll(PDO::FETCH_ASSOC);
+                }
 
-            foreach ($agents as $agent)
-            {
-                echo "<div class='ui centered small image'>
-                <img id='" . $agent['AgtLastName']. "' src='" . $agent['Image'] . "'>
-                </div>
-                <div class='content'>
-                <a class='header'>" . $agent['AgtFirstName'] . $agent['AgtLastName'] . "</a>
-                <div class='meta'>
-                <span class='date'>" . $agent['AgtPosition'] . "</span>
-                </div>
-                <div class='description'>" . $agent['Description'] . "</div>
-                </div>
-                <div class='extra'>
-                Rating:
-                <div id='rating1' class='ui star rating'></div>
-                </div>
-                </div>";
-            }    
-        ?>
+                
+                $counter = 1;
+                
+                foreach ($agents as $agent)
+                {
+                    // echo '
+                    // <section class="company-info">
+                    //     <ul class="agency-info">
+                    //         <!-- Company Contact Information -->
+                    //         <h2><b>Agency Name: Classical Travel</b></h2>
+                    //         <p>Address: 123 Love Street, Heaven</p>
+                    //     </ul>
+                    // </section>
+                    //     '
+                    
+                    echo '
+                    <div id="contactCard'.$counter.'" class="ui card">
+                        <!-- MODAL CODE -->
+                        <div id="modal'.$counter.'" class="ui modal test">
+                            <div class="header">
+                                Agent Contact Information
+                            </div>
+                            <div class="image content">
+                                <div class="ui small centered image">
+                                    <img src="'.$agent['Image'].'">
+                                </div>
+                                <div class="description">
+                                    <div class="ui header">'.$agent['AgtFirstName'].'" "'.$agent['AgtLastName'].'</div>
+                                    <!-- Agent Contact Information -->
+                                    <p>Phone#: '.$agent['AgtBusPhone'].'</p>
+                                    <p>Email: '.$agent['AgtEmail'].'</p>
+                                </div>
+                            </div>
+
+                            <div class="actions">
+                                <div class="ui black deny button">
+                                    Back
+                                </div>
+                                <div class="ui positive right labeled icon button">
+                                    <a href="contactAgent.php">Contact Agent</a>
+                                </div>
+
+                            </div>
+                        </div>
+                        <!-- CONTACT CARD 1 -->
+                        <div class="ui centered small image">
+                            <img id="'.$agent['AgtLastName'].'" src="'.$agent['Image'].'">
+                        </div>
+                        <div class="content">
+                            <a class="header">'.$agent['AgtFirstName'].'" "'.$agent['AgtLastName'].'</a>
+                            <div class="meta">
+                                <span class="date">'.$agent['AgtPosition'].'</span>
+                            </div>
+                            <div class="description">
+                            '.$agent['Description'].'
+                            </div>
+                        </div>
+                        <div class="extra">
+                            Rating:
+                            <div id="rating'.$counter.'" class="ui star rating"></div>
+                        </div>
+                    </div>
+                    ';
+
+                    $counter++; //counter for each contact card and modal
+                }
+            ?>
+
         </div>
     </div>
 </main>
